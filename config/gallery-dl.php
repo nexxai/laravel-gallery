f66083f503e864a00b690dca5af0689b1572413c
JT Smith <nexxai@gmail.com>
2026-03-30 14:45:38 -0600

    fix: default download location

--- config/gallery-dl.php
+++ config/gallery-dl.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'binary_path' => base_path('tools/gallery-dl' . (PHP_OS_FAMILY === 'Windows' ? '.exe' : '')),
+    'binary_path' => base_path('tools/gallery-dl'.(PHP_OS_FAMILY === 'Windows' ? '.exe' : '')),
 
     'binary_download_url' => env(
         'GALLERY_DL_BINARY_DOWNLOAD_URL',
         PHP_OS_FAMILY === 'Windows'
             ? 'https://github.com/mikf/gallery-dl/releases/latest/download/gallery-dl.exe'
-            : 'https://github.com/mikf/gallery-dl/releases/latest/download/gallery-dl'
+            : 'https://github.com/mikf/gallery-dl/releases/latest/download/gallery-dl.bin'
     ),
 
     'download_path' => storage_path('gallery'),
